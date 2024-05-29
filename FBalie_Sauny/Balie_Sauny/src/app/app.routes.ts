d89d569c036bcc94e99b8e179e5ff71ed464aa94
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-05-29 21:51:11 +0200

    working routes in application

--- FBalie_Sauny/Balie_Sauny/src/app/app.routes.ts
+++ FBalie_Sauny/Balie_Sauny/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ShopComponent } from './shop/shop.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: MainPageComponent},
+    { path: 'shop', component: ShopComponent},
+    { path: 'shop/:id', component: ProductDetailComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
