4defa32c970f07c0cc3ab55bf78cdb0c073b8509
DESKTOP-TN3VE9B\szymo <szymonzaba75@gmail.com>
2024-06-05 00:15:09 +0200

    working view of reservated dates and free dates plus selcting dates

--- FBalie_Sauny/Balie_Sauny/src/app/product-detail/product-detail.component.ts
+++ FBalie_Sauny/Balie_Sauny/src/app/product-detail/product-detail.component.ts
@@ -5,10 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReservationService } from '../services/reservation.service';
 import { RouterModule } from '@angular/router';
 import { TubService } from '../services/tub.service';
-import { FullCalendarModule } from '@fullcalendar/angular'; // Import FullCalendar module
-import { CalendarOptions } from '@fullcalendar/core'; // Import CalendarOptions
-import dayGridPlugin from '@fullcalendar/daygrid'; // Import the dayGrid plugin
-import interactionPlugin from '@fullcalendar/interaction'; // Import the interaction plugin
+import { FullCalendarModule } from '@fullcalendar/angular'; 
+import { CalendarOptions } from '@fullcalendar/core'; 
+import dayGridPlugin from '@fullcalendar/daygrid'; 
+import interactionPlugin from '@fullcalendar/interaction'; 
+
+interface CalendarEvent {
+    title: string;
+    start: Date;
+    end?: Date;
+    allDay: boolean;
+    display: string;
+    backgroundColor: string;
+    classNames?: string[];
+}
 
 @Component({
     selector: 'app-product-detail',
@@ -24,11 +34,14 @@ export class ProductDetailComponent implements OnInit {
     Date1: Date = new Date();
     tub: any = {};
     selectedDates: Date[] = [];
+    currentStartDate!: Date;
+    currentEndDate!: Date;
     calendarOptions: CalendarOptions = {
         initialView: 'dayGridMonth',
         plugins: [dayGridPlugin, interactionPlugin],
         dateClick: this.handleDateClick.bind(this),
-        events: [] // Initial empty events
+        events: [],
+        datesSet: this.handleDatesSet.bind(this) // Handle date changes
     };
 
     constructor(
@@ -50,6 +63,7 @@ export class ProductDetailComponent implements OnInit {
         this.reservationService.getReservationByTub(this.tubId).subscribe(data => {
             this.reservation = data;
             console.log(this.reservation);
+            this.updateCalendarReservations(this.normalizeDate(new Date()), this.normalizeDate(new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)));
         });
     }
 
@@ -61,13 +75,13 @@ export class ProductDetailComponent implements OnInit {
 
     handleDateClick(arg: any) {
         if (this.selectedDates.length < 2) {
-            this.selectedDates.push(arg.date);
+            this.selectedDates.push(this.normalizeDate(arg.date));
         } else {
-            this.selectedDates = [arg.date];
+            this.selectedDates = [this.normalizeDate(arg.date)];
         }
 
-        // Update calendar events
-        this.updateCalendarEvents();
+        // Update calendar events to show selected dates
+        this.updateCalendarReservations(this.currentStartDate!, this.currentEndDate!);
 
         // Print date information if two dates are selected
         if (this.selectedDates.length === 2) {
@@ -75,26 +89,87 @@ export class ProductDetailComponent implements OnInit {
         }
     }
 
-    updateCalendarEvents() {
-        const events = this.selectedDates.map(date => ({
+    handleDatesSet(arg: any) {
+        this.currentStartDate = arg.start;
+        this.currentEndDate = arg.end;
+        this.updateCalendarReservations(arg.start, arg.end);
+    }
+
+    updateCalendarReservations(startDate: Date, endDate: Date) {
+        let reservationEvents: CalendarEvent[] = [];
+
+        this.reservation.forEach(res => {
+            const start = this.normalizeDate(new Date(res.start_date));
+            const end = this.normalizeDate(new Date(res.end_date));
+            end.setDate(end.getDate() + 1); // Add one day to include the end date
+
+            reservationEvents.push({
+                title: '',
+                start: start,
+                end: end,
+                allDay: true,
+                display: 'background',
+                backgroundColor: 'lightcoral',
+                classNames: ['fc-event-lightcoral']
+            });
+        });
+
+        const allDaysInView = this.getAllDaysInView(startDate, endDate);
+
+        let freeDays = allDaysInView.filter(day => 
+            !reservationEvents.some(event => 
+                event.start <= day && event.end! > day
+            )
+        );
+
+        let freeEvents: CalendarEvent[] = freeDays.map(day => ({
+            title: '',
+            start: day,
+            allDay: true,
+            display: 'background',
+            backgroundColor: 'lightgreen',
+            classNames: ['fc-event-lightgreen']
+        }));
+
+        // Add selected dates on top of other events
+        let selectedEvents: CalendarEvent[] = this.selectedDates.map(date => ({
             title: 'Selected',
             start: date,
             allDay: true,
             display: 'background',
-            backgroundColor: 'yellow'
+            backgroundColor: 'yellow',
+            classNames: ['fc-event-selected']
         }));
 
         this.calendarOptions = {
             ...this.calendarOptions,
-            events: events
+            events: [...reservationEvents, ...freeEvents, ...selectedEvents]
         };
+    }
+
+    getAllDaysInView(startDate: Date, endDate: Date): Date[] {
+        let dates = [];
+        let currentDate = new Date(startDate);
+
+        while (currentDate <= endDate) {
+            dates.push(new Date(currentDate));
+            currentDate.setDate(currentDate.getDate() + 1);
+        }
+
+        return dates;
+    }
+
+    normalizeDate(date: Date): Date {
+        const normalizedDate = new Date(date);
+        normalizedDate.setHours(0, 0, 0, 0);
+        return normalizedDate;
     }
 
     printDateInfo() {
         const startDate = this.selectedDates[0];
         const endDate = this.selectedDates[1];
         const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
-        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1; // Including both start and end date
+        const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)) + 1; 
 
         console.log('Start Date:', startDate);
         console.log('End Date:', endDate);
