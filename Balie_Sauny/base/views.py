675e57432f5e189de6d74a6722f96ebbff66db86
Szabson123 <szymonzaba18@gmail.com>
2024-06-24 14:48:29 +0200

    few changes

--- Balie_Sauny/base/views.py
+++ Balie_Sauny/base/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status, generics
+from rest_framework import viewsets, status, generics, permissions
 from .models import Tub, Reservation, Rating, Discount, Faq
 from .serializers import TubSerializer, ReservationSerializer, RatingSerializer, DiscountSerializer, FaqSerializer, AddTubSerializer, Address
 from django.shortcuts import get_object_or_404
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from decimal import Decimal
 from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import PermissionDenied
 
 
 class TubViewListSet(viewsets.ModelViewSet):
